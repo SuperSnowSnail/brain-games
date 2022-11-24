6c22b48aec25e425a2adcac4b7b0ec873700bc8c
SnowSnail <f2961@yandex.ru>
2022-11-24 22:42:21 +0300

    Add CodeClimate maintainability test and ESLint

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/SuperSnowSnail/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/SuperSnowSnail/frontend-project-44/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/4a961cc53b94b61de815/maintainability)](https://codeclimate.com/github/SuperSnowSnail/frontend-project-44/maintainability)
--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 const welcomeUser = () => {
-  console.log("Welcome to the Brain Games!");
-  const userName = readlineSync.question("May I have your name? ");
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
 
-export { welcomeUser };
+export default welcomeUser;
