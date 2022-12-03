0b35e0d3c911f951cea8c3c1c0fec14c494233a4
SnowSnail <f2961@yandex.ru>
2022-12-03 18:34:38 +0300

    Brain GCD Game refactoring

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,32 +1,32 @@
-import gameEngine from '../index.js';
-import { arrayGenerator, randomNumberFromRange } from '../utils.js';
+import runGameEngine from '../index.js';
+import getRandomNumInRange from '../utils.js';
 
-const gameCondition = 'Find the greatest common divisor of given numbers.';
+const gameDescription = 'Find the greatest common divisor of given numbers.';
 
-const questionFunction = () => {
-  const firstNumber = randomNumberFromRange(1, 20);
-  const secondNumber = randomNumberFromRange(1, 20);
-  return `${firstNumber} ${secondNumber}`;
-};
-
-const answerFunction = (i, questions) => {
-  const question = questions[i].split(' ');
-  let firstNumber = Number(question[0]);
-  let secondNumber = Number(question[1]);
-  while (firstNumber !== secondNumber) {
-    if (firstNumber > secondNumber) {
-      firstNumber -= secondNumber;
+const calculateGcd = (num1, num2) => {
+  let firstNum = num1;
+  let secondNum = num2;
+  while (firstNum !== secondNum) {
+    if (firstNum > secondNum) {
+      firstNum -= secondNum;
     } else {
-      secondNumber -= firstNumber;
+      secondNum -= firstNum;
     }
   }
-  return String(firstNumber);
+
+  return firstNum;
 };
 
-const gcdGame = () => {
-  const questions = arrayGenerator(questionFunction);
-  const rightAnswers = arrayGenerator(answerFunction, questions);
-  gameEngine(questions, rightAnswers, gameCondition);
+const generateRound = () => {
+  const firstNum = getRandomNumInRange(1, 20);
+  const secondNum = getRandomNumInRange(1, 20);
+
+  const question = `${firstNum} ${secondNum}`;
+  const answer = calculateGcd(firstNum, secondNum);
+
+  return [question, answer];
 };
 
-export default gcdGame;
+export default () => {
+  runGameEngine(gameDescription, generateRound);
+};
